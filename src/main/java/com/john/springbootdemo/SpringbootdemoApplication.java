c84c3e436d03fe03224563f9f73559e61d2637f0
John <li.yang@ubtrobot.com>
2019-08-06 14:51:53 +0800

    将打包方式修改为jar方式

--- src/main/java/com/john/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/john/springbootdemo/SpringbootdemoApplication.java
@@ -6,16 +6,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootdemoApplication extends SpringBootServletInitializer{
-
+public class SpringbootdemoApplication {
+	//extends SpringBootServletInitializer war第一步
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
-	//重写此方法为了打包到linux
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SpringbootdemoApplication.class);
-	}
+//	//重写此方法为了打包到linux 打war包时用到
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(SpringbootdemoApplication.class);
+//	}
 }
 
